d4238e41e4bd9b70f535cacdc3168a62f54e4ce4
Kelly Anne Pipe <kellyanne.martin@gmail.com>
2020-01-13 09:49:28 -0500

    Updates

---
+++ precache-manifest.f482f4444a772c4e6208d7b7fd9f0b4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf3027d5eb05e3991dc63cfa81fd560",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "eafa89af5b3c68ef6725",
+    "url": "/emoji-search/static/css/2.ed45abbb.chunk.css"
+  },
+  {
+    "revision": "b5a33ac3b33af4f77e17",
+    "url": "/emoji-search/static/css/main.ba6fb9ef.chunk.css"
+  },
+  {
+    "revision": "eafa89af5b3c68ef6725",
+    "url": "/emoji-search/static/js/2.5b0856a9.chunk.js"
+  },
+  {
+    "revision": "bb35913824c0ec513bf4cd9422cf95c6",
+    "url": "/emoji-search/static/js/2.5b0856a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5a33ac3b33af4f77e17",
+    "url": "/emoji-search/static/js/main.7068db6a.chunk.js"
+  },
+  {
+    "revision": "33664a7b5f6ec4e4d24a",
+    "url": "/emoji-search/static/js/runtime-main.a2c260c4.js"
+  }
+]);
